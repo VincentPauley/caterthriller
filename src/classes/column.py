e0447335cdbf2cc5010c5bcf10609e71b02e3b6e
Vinnie Pauley <vinniepauley@Vinnies-Mac-mini.local>
2026-02-01 10:58:40 -0600

    feat: column update

--- src/classes/column.py
+++ src/classes/column.py
@@ -6,11 +6,11 @@ import pygame
 class Column(pygame.sprite.Sprite):
     def __init__(self, topLeftPos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("src/graphics/column-v1.png").convert()
+        self.image = pygame.image.load("src/graphics/rock-column-1.png").convert_alpha()
         self.top_left_pos = topLeftPos
         self.rect = self.image.get_rect(topleft=self.top_left_pos)
 
     def update(self, y_pos_change):
         self.top_left_pos.y = y_pos_change
-        # round y pos for rendering
-        self.rect.y = round(self.top_left_pos.y)
+        # round y pos for rendering to avoid sub-pixel rendering issues
+        self.rect.y = int(self.top_left_pos.y)
