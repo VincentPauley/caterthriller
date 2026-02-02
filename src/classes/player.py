2bdf29087e042f3d441f11ffb29947f174715959
Vinnie Pauley <vinniepauley@Vinnies-Mac-mini.local>
2026-02-02 13:01:54 -0600

    feat: player movement accel and decel

--- src/classes/player.py
+++ src/classes/player.py
@@ -7,24 +7,40 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('src/graphics/player-head.png').convert_alpha()
         self.rect = self.image.get_rect(center=(pos))
         self.pos = pygame.math.Vector2(self.rect.center)
-        self.direction = pygame.math.Vector2(0,0)
-        self.speed = 400
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.max_speed = 600
+        self.acceleration = 4000  # pixels per second squared
+        self.friction = 3000  # deceleration when no input
 
     def move(self, dt):
-        self.pos.x += self.direction.x * self.speed * dt
-
+        # Apply velocity to position
+        self.pos.x += self.velocity.x * dt
         self.rect.centerx = int(self.pos.x)
 
-    def input(self):
+    def input(self, dt):
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
+            # Accelerate right
+            self.velocity.x += self.acceleration * dt
+            if self.velocity.x > self.max_speed:
+                self.velocity.x = self.max_speed
         elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
+            # Accelerate left
+            self.velocity.x -= self.acceleration * dt
+            if self.velocity.x < -self.max_speed:
+                self.velocity.x = -self.max_speed
         else:
-            self.direction.x = 0
+            # Apply friction when no input
+            if self.velocity.x > 0:
+                self.velocity.x -= self.friction * dt
+                if self.velocity.x < 0:
+                    self.velocity.x = 0
+            elif self.velocity.x < 0:
+                self.velocity.x += self.friction * dt
+                if self.velocity.x > 0:
+                    self.velocity.x = 0
         
     def update(self, dt):
-        self.input()
+        self.input(dt)
         self.move(dt)
