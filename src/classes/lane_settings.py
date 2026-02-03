6512aa1b50ef0592ff500a7116060659b8818c67
Vinnie Pauley <vinniepauley@Vinnies-Mac-mini.local>
2026-02-02 18:40:19 -0600

    feat: add player spots to the game board

--- src/classes/lane_settings.py
+++ src/classes/lane_settings.py
@@ -10,3 +10,11 @@ class LaneSettings:
             x_positions.append(i * self.lane_width + self.lane_width)  # one extra for padding
 
         return x_positions
+    
+    def get_lane_center_x_positions(self):
+        x_positions = []
+
+        for i in range(self.lane_count):
+            x_positions.append(i * self.lane_width + self.lane_width + self.lane_width / 2)
+
+        return x_positions
--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import pygame
 
 from classes.lane_settings import LaneSettings
 from classes.wall_management import WallManagement
+from classes.place_marker import PlaceMarker
 from classes.player import Player
 from settings import WINDOW_HEIGHT, WINDOW_WIDTH
 
-all_sprites = pygame.sprite.Group()
 
 pygame.init()
 display_surface = pygame.display.set_mode(
@@ -14,6 +14,7 @@ display_surface = pygame.display.set_mode(
 pygame.display.set_caption("Caterthriller")
 clock = pygame.time.Clock()
 
+place_markers = pygame.sprite.Group()
 player_sprites = pygame.sprite.Group()
 
 wall_manager = WallManagement(display_surface)
@@ -24,6 +25,10 @@ running = True
 bg = pygame.image.load("src/graphics/background.png").convert()
 
 lane_settings = LaneSettings()
+
+# show player spots
+for x_pos in lane_settings.get_lane_center_x_positions():
+    PlaceMarker(pygame.math.Vector2(x_pos, 500), place_markers)
 
 while running:
     for event in pygame.event.get():
@@ -37,6 +42,7 @@ while running:
     wall_manager.update(dt)
     player_sprites.update(dt)
     player_sprites.draw(display_surface)
+    place_markers.draw(display_surface)
 
     pygame.display.flip()
 
