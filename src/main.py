769b3701f7868a7127e5cdbe898d3e04ce8ba9d0
Vinnie Pauley <vinniepauley@gmail.com>
2026-02-07 15:34:01 -0600

    feat: water effects mapped out

--- src/main.py
+++ src/main.py
@@ -55,8 +55,10 @@ water_sprites = pygame.sprite.Group()
 WaterEdge(pygame.math.Vector2(0, 0), [water_sprites])
 WaterEdge(pygame.math.Vector2(576, 0), [water_sprites], True)
 
-for x in settings.game.lanes.x_positions:
-    water_sprite = WaterLane(pygame.math.Vector2(x, 0), water_sprites)
+for i, lx in enumerate(settings.game.lanes.x_positions):
+    if i > 0 and i < len(settings.game.lanes.x_positions) - 1:
+        WaterLane(pygame.math.Vector2(lx, 0), water_sprites)
+
 
 while running:
     for event in pygame.event.get():
